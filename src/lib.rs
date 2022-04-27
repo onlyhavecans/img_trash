0dc5387521ac646ff064efea90f479108c9aa3da
Amelia Aronsohn <squirrel@wearing.black>
2022-04-27 13:20:22 -0700

    Fix path errors in lib, update deps

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use image::DynamicImage;
-use std::env;
 use std::path::Path;
 use std::process::Command;
+use std::{env, fs};
 
 pub fn get_files_from_args() -> Option<Vec<String>> {
     let files = env::args()
@@ -16,15 +16,19 @@ pub fn get_files_from_args() -> Option<Vec<String>> {
 
 pub fn save_and_open_file(old_filename: &Path, image_data: &DynamicImage) {
     let filename = old_filename.file_stem().unwrap().to_str().unwrap();
-    let new_file = format!("img_out/{}.png", filename);
+    let new_file = format!("{}.png", filename);
     let new_file = new_file.as_str();
-    let new_file_path = Path::new(new_file);
+    let out_path = Path::new("img_out");
+    let new_file_path = out_path.join(Path::new(new_file));
 
     println!("Writing file {}", new_file);
 
-    image_data.save(new_file_path).unwrap();
+    fs::create_dir_all(out_path).expect("Could not create output directory");
+    image_data
+        .save(new_file_path.as_os_str())
+        .expect("Could not save output");
     Command::new("open")
-        .arg(new_file)
+        .arg(new_file_path)
         .spawn()
         .expect("couldn't open");
 }
